5b199ecdc9ed727c5098918de3283af735b10d96
LIAMYUZEDONG <yyyzzddd@gmail.com>
2022-02-16 12:05:44 -0700

    m7

---
+++ Mission7 Assignment/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using BookProject.Models;
+using BookProject.Models.ViewModels;
+
+namespace BookProject.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+        private IBookRepository repo;
+      
+
+        public HomeController (IBookRepository temp)
+        {
+            repo = temp;
+        }
+
+        public IActionResult Index(int pageNum = 1) // do not use "page" as variable name
+        {
+            int NumBooks = 10;
+
+            var x = new ProjectsViewModel
+            {
+                Books = repo.Books
+                .OrderBy(p => p.Title)
+                .Skip((pageNum - 1) * NumBooks)
+                .Take(NumBooks),
+
+                PageInfo = new PageInfo
+            {
+                TotalNumProjects = repo.Books.Count(),
+                ProjectsPerPage = NumBooks,
+                CurrentPage = pageNum
+            }
+
+            };
+
+     
+
+              return View(x);
+      
+        }
+    }
+}
---
+++ Mission7 Assignment/Models/Projects.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace BookProject.Models
+{
+    public partial class Book
+    {
+        [Key]
+        [Required]
+        //public long ProjectId { get; set; }
+        //public string ProjectName { get; set; }
+        //public string ProjectType { get; set; }
+        //public string ProjectRegionalProgram { get; set; }
+        //public int ProjectImpact { get; set; }
+        //public string ProjectPhase { get; set; }
+        //public string ProjectFunctionalityStatus { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        public string Category { get; set; }
+        public string Classification { get; set; }
+        public int PageCount { get; set; }
+        public int Price { get; set; }
+        public int ISBN { get; set; }
+
+
+
+    }
+}
